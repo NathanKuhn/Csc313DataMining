171c71a92a362e833c785bcc98fa74c4e0e71b89
NathanKuhn <kuhnnat000@gmail.com>
2025-02-04 13:42:48 -0800

    Fix part-1 and add answers

--- max_compiler_errors.py
+++ max_compiler_errors.py
@@ -6,24 +6,38 @@ MAIN_TABLE = "dataset/Data/MainTable.csv"
 
 def max_compiler_errors():
     errors_counter = Counter()
+    students_per_problem = {}
 
     with open(MAIN_TABLE, "r") as main_file:
         main_reader = csv.reader(main_file)
         next(main_reader)  # Skip header line
 
         for row in main_reader:
+            student_id = row[1]
             problem_id = row[8]
             has_compiler_error = row[11] == "Compile" and row[13] == "Error"
 
             if has_compiler_error:
                 errors_counter[problem_id] += 1
 
-    max_problem = max(errors_counter, key=errors_counter.get)
-    max_count = errors_counter[max_problem]
+                if problem_id not in students_per_problem:
+                    students_per_problem[problem_id] = set()
+                
+                students_per_problem[problem_id].add(student_id)
 
-    return max_problem, max_count
+
+    max_problem = None
+    max_avg = None
+    for problem in errors_counter.keys():
+        avg = errors_counter[problem] / len(students_per_problem[problem])
+
+        if not max_problem or max_avg < avg:
+            max_problem = problem
+            max_avg = avg
+
+    return max_problem, max_avg
 
 
 if __name__ == "__main__":
     max_problem, max_count = max_compiler_errors()
-    print(f"Problem with maximum compiler errors: {max_problem} with {max_count} errors")
+    print(f"Problem with maximum compiler errors: {max_problem} with {max_count:0.2f} errors")
--- max_problem_attempts.py
+++ max_problem_attempts.py
@@ -7,35 +7,54 @@ LATE_FILE = "dataset/late.csv"
 
 def max_problem_attempts():
     attempts_counter = Counter()
+    students_per_problem = {}
 
     with open(EARLY_FILE, "r") as early_file:
         early_reader = csv.reader(early_file)
         next(early_reader)  # Skip header line
 
         for row in early_reader:
+            student_id = row[0]
             assignment_id = row[1]
             problem_id = row[2]
             attempts = int(row[3])
 
             attempts_counter[problem_id] += attempts
+
+            if problem_id not in students_per_problem:
+                students_per_problem[problem_id] = set()
+            
+            students_per_problem[problem_id].add(student_id)
 
     with open(LATE_FILE, "r") as late_file:
         late_reader = csv.reader(late_file)
         next(late_reader)  # Skip header line
 
         for row in late_reader:
+            student_id = row[0]
             assignment_id = row[1]
             problem_id = row[2]
             attempts = int(row[3])
 
             attempts_counter[problem_id] += attempts
 
-    max_problem = max(attempts_counter, key=attempts_counter.get)
-    max_count = attempts_counter[max_problem]
+            if problem_id not in students_per_problem:
+                students_per_problem[problem_id] = set()
+            
+            students_per_problem[problem_id].add(student_id)
 
-    return max_problem, max_count
+    max_problem = None
+    max_avg = None
+    for problem in attempts_counter.keys():
+        avg = attempts_counter[problem] / len(students_per_problem[problem])
+
+        if not max_problem or max_avg < avg:
+            max_problem = problem
+            max_avg = avg
+
+    return max_problem, max_avg
 
 
 if __name__ == "__main__":
     max_problem, max_count = max_problem_attempts()
-    print(f"Problem with maximum attempts: {max_problem} with {max_count} attempts")
+    print(f"Problem with maximum attempts: {max_problem} with {max_count:0.2f} attempts")
